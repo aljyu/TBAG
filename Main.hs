802ec7b85e1a550a70587a3c8b832f69f581cfca
aljyu <aljyu@ucsc.edu>
2016-03-02 17:23:35 -0800

    Got HP decrementation working

--- Main.hs
+++ Main.hs
@@ -1,65 +1,54 @@
-module Main where
+module Main (main) where
 import Player
 
 -- fuction for printing out status
-status :: Player -> IO() 
-status (Player {hp = h, potions = p}) = print ("Player has " ++ (show h) ++ " HP and " ++ (show p) ++ " potions" ) 
+status :: Player -> IO()
+status (Player {hp = h, potions = p}) = print ("Player has " ++ (show h) ++ " HP and " ++ (show p) ++ " potions" )
 
--- global player object
+-- "global" variable
+player :: Player
 player = Player 100 0
 
-main = do  
-  putStrLn("Welcome to EK4!")
-  putStrLn("What is your name?")
-  n <- getLine
-  putStrLn("\nHello " ++ n ++ "!\n")
-   
-  --player function (set name, ...)
-  --player 
-  --putStrLn $ status player 
-  putStrLn "If you wish to view these stats during any point of the game, type in \"STATUS\"." 
-  putStrLn("If you wish to drink a potion, first type in Status and follow the instructions given.")
-  putStrLn("In this game, every time you perform an action you will lose HP. When your HP hits 0, you will die and the game will end.")
-  putStrLn("Now, let's begin...")
+main = do
+                putStrLn("Welcome to EK4!")
+                putStrLn("What is your name?")
+                name <- getLine
+                putStrLn("\nHello " ++ name ++ "!\n") 
+                putStrLn "If you wish to view these stats during any point of the game, type in \"STATUS\"." 
+                putStrLn("If you wish to drink a potion, first type in Status and follow the instructions given.")
+                putStrLn("In this game, every time you perform an action you will lose HP. When your HP hits 0, you will die and the game will end.")
+                putStrLn("Now, let's begin...")
 
-  --if (stat True) then 
+                putStrLn("--------------------------------------------------------------------------------------------------------------------------------------------")
+                putStrLn("                                                                Level 1");
+                putStrLn("--------------------------------------------------------------------------------------------------------------------------------------------")
 
-  putStrLn("--------------------------------------------------------------------------------------------------------------------------------------------")
-  putStrLn("                                                                Level 1");
-  putStrLn("--------------------------------------------------------------------------------------------------------------------------------------------")
+                putStrLn("ZZZzzz...\nYou are sleeping peacefully, dreaming of summer vacation, when suddenly you are awakened by a bright, annoying light flashing through the window, directly at your face.")
+                putStrLn("\"Why is it so bright...?!\" you gripe as you pull your covers over your head, trying to ignore the light and fall back asleep.")
+                putStrLn("However, your attempts turn out to be futile as you begin to hear a series of high-pitched noises.")
+                putStrLn("You decide that you've had enough and get out of bed to check the window for the source of the disturbance, only to realize that the light shining through your window is making it too bright to see anything outside.")
+                putStrLn("As you stand next to your window clad only in your PJs wondering what you should do, the high-pitched noises begin to fade as you hear a voice.")
+                putStrLn("\"" ++ name ++ "\" a soft, gentle voice calls out.")
+                putStrLn("You look around, trying to pinpoint the location of the voice.")
+                putStrLn("\"This voice is hauntingly beautiful... and... familiar...?\" you think as you being to feel compelled to track down its owner.")
+                putStrLn("As you begin to walk out of your room, the light behind you begins to slowly fade away.\n")
+                doChoice1 player
 
-  putStrLn("ZZZzzz...\nYou are sleeping peacefully, dreaming of summer vacation, when suddenly you are awakened by a bright, annoying light flashing through the window, directly at your face.")
-  putStrLn("\"Why is it so bright...?!\" you gripe as you pull your covers over your head, trying to ignore the light and fall back asleep.")
-  putStrLn("However, your attempts turn out to be futile as you begin to hear a series of high-pitched noises.")
-  putStrLn("You decide that you've had enough and get out of bed to check the window for the source of the disturbance, only to realize that the light shining through your window is making it too bright to see anything outside.")
-  putStrLn("As you stand next to your window clad only in your PJs wondering what you should do, the high-pitched noises begin to fade as you hear a voice.")
-  putStrLn("\"" ++ n ++ "\" a soft, gentle voice calls out.")
-  putStrLn("You look around, trying to pinpoint the location of the voice.")
-  putStrLn("\"This voice is hauntingly beautiful... and... familiar...?\" you think as you being to feel compelled to track down its owner.")
-  putStrLn("As you begin to walk out of your room, the light behind you begins to slowly fade away.\n")
-  --let player2 = move player 
-  goToOptions1
-  --LOOP1
-  --x <- getLine
-  --stat x
-  --check if input calls status
+doChoice1 character = do
+                                        let currentPlayer = keep character
+                                        putStrLn "\"Alright\" you think to yourself \"Where should I check out first? Should I check out the KITCHEN, check out the HOUSE, or check out the BACKYARD?\""
+                                        input <- getLine
+                                        let newPlayer = move currentPlayer
+                                        --if input == "KITCHEN" then do goToKitchen
+                                        --else if input == "DOG HOUSE" then do goToDogHouse 
+                                        --else if input == "BACKYARD" then do goToBackyard
+                                        if input == "status" then do status newPlayer
+                                        else putStrLn "Command not recognized. Please try again.\n"
+                                        let character = newPlayer
+                                        --status player
+                                        doChoice1 character
 
--- options for first choices
-goToOptions1 = do
-     putStrLn "\"Alright\" you think to yourself \"Where should I check out first? Should I check out the KITCHEN, check out the HOUSE, or check out the BACKYARD?\""
-     input <- getLine
-     let player = move player
-     if input == "KITCHEN"
-       then do goToKitchen
-       else if input == "DOG HOUSE"
-              then do goToDogHouse 
-              else if input == "BACKYARD"
-                   then do goToBackyard
-                   else if input == "status"
-                      then do (status player)
-                      else do putStrLn "Command not recognized. Please try again.\n"
-                              goToOptions1
-
+{-
 -- otions if you go to the kitchen
 goToKitchen = do
    putStrLn "\nYou walk into the kitchen.\n\"Alright\" you think to yourself \"Do I want to LOOK INSIDE THE SINK, CHECK OUT THE DOG HOUSE, or CHECK OUT THE BACKYARD"
@@ -151,3 +140,4 @@ goToLevel2 = do
   --else 
     --if (input == "LOOK INSIDE THE SINK " ) then currentHP - 5
     -- if current HP <= 0 then gamestat false
+-}
--- Player.hs
+++ Player.hs
@@ -1,24 +1,22 @@
 module Player
 (
   Player(..)
+  ,keep
   ,move
   ,hit
   ,drink
 ) where
 
 data Player = Player{ hp :: Int, potions :: Int} deriving (Show,Eq)
- 
---tellPlayer :: Player -> String  
---tellPlayer (Player {hp = h, potions = p}) = "Player has " ++ show h ++ " HP and " ++ show p ++ " potions"  
 
-move::Player->Player
+keep :: Player -> Player
+keep hero = Player (hp hero) (potions hero)
+
+move :: Player -> Player
 move hero  = Player ((hp hero) - 5) (potions hero)
 
-hit::Player->Player
-hit hero  = Player ((hp hero) - 20) (potions hero)
+hit :: Player -> Player
+hit hero = Player ((hp hero) - 20) (potions hero)
 
-drink::Player->Player
+drink :: Player -> Player
 drink hero  = Player ((hp hero) + 20) (potions hero)
-
-
-
