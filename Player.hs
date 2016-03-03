3f3cd5a2f506b1c82b264b0200167902908d9781
aljyu <aljyu@ucsc.edu>
2016-03-02 21:10:33 -0800

    Almost done with Level 1

--- Player.hs
+++ Player.hs
@@ -1,22 +1,22 @@
 module Player
 (
   Player(..)
-  ,keep
   ,move
+  ,extra
   ,hit
   ,drink
 ) where
 
 data Player = Player{ hp :: Int, potions :: Int} deriving (Show,Eq)
 
-keep :: Player -> Player
-keep hero = Player (hp hero) (potions hero)
-
 move :: Player -> Player
 move hero  = Player ((hp hero) - 5) (potions hero)
+
+extra :: Player -> Player
+extra hero = Player (hp hero) ((potions hero) + 1)
 
 hit :: Player -> Player
 hit hero = Player ((hp hero) - 20) (potions hero)
 
 drink :: Player -> Player
-drink hero  = Player ((hp hero) + 20) (potions hero)
+drink hero  = Player ((hp hero) + 20) ((potions hero) - 1)
