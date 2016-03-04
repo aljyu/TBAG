b39db34a759794cba82464c0db54900a1eb46812
= <=>
2016-03-04 11:57:54 -0800

    Added HP limitation for life and death

--- Main.hs
+++ Main.hs
@@ -8,6 +8,9 @@ status (Player {hp = h, potions = p}) = do
 
 numPots :: Player -> String
 numPots (Player {hp = h, potions = p}) = show p
+
+isAlive :: Player -> Bool
+isAlive (Player {hp = h, potions = p}) = if h > 0 then True else False
  
 -- "global" variable
 player :: Player
@@ -65,6 +68,7 @@ doChoice1 character = do
 
 goToDogHouse str character = do
                                                     let newPlayer = move character
+                                                    if (isAlive newPlayer == False) then gameOverFail else do
                                                     putStrLn "\n\"Blue?\" you call out as you walk out the front door towards the dog house."
                                                     putStrLn "You hear your dog, Blue, trotting towards you with something in his mouth."
                                                     putStrLn "\"Should I take the ITEM, check out the KITCHEN, or check out the BACKYARD?\" you wonder to yourself as you absent-mindedly pet Blue on the head."
@@ -95,11 +99,13 @@ goToDogHouse str character = do
 takeItem str character
     | str == "true" =   do
                                     let newPlayer = extra character
+                                    if (isAlive newPlayer == False) then gameOverFail else do
                                     putStrLn "You remove the item from Blue's mouth.\nYOU HAVE OBTAINED A POTION!"
                                     let str = "false"
                                     goToDogHouse str newPlayer
     | str == "false"    =   do
                                         let newPlayer = move character
+                                        if (isAlive newPlayer == False) then gameOverFail else do
                                         putStrLn "You remove the item from Blue's mouth only to find out that it is an old tennis ball."
                                         goToDogHouse str character
     | otherwise         =   do
@@ -108,6 +114,7 @@ takeItem str character
 
 goToKitchen str character = do
                                                 let newPlayer = move character
+                                                if (isAlive newPlayer == False) then gameOverFail else do
                                                 putStrLn "\nYou walk into the kitchen.\n\"Alright\" you think to yourself \"Do I want to look inside the SINK, check out the DOG HOUSE, or check out the BACKYARD?\""
                                                 input <- getLine
                                                 if input == "STATUS" then do
@@ -135,6 +142,7 @@ goToKitchen str character = do
 
 goToSink str character = do
                                             let newPlayer = move character
+                                            if (isAlive newPlayer == False) then gameOverFail else do
                                             putStrLn "\nYou look over to your sink and find a bunch of dirty dishes. \"I should've cleaned those hours ago...\" you think to yourself"
                                             putStrLn "\"Do I want to check out the DOG HOUSE, or check out the BACKYARD\"?\n"
                                             input <- getLine
@@ -162,6 +170,7 @@ goToSink str character = do
 
 goToBackyard character = do
                                                 let newPlayer = move character
+                                                if (isAlive newPlayer == False) then gameOverFail else do
                                                 putStrLn "\nYou walk outside to the backyard where you are immediately caught in a tractor beam."
                                                 putStrLn "Your thoughts immediately go into overdrive mode, debating whether it would be better to STRUGGLE against the beam or just COMPLY."
                                                 input <- getLine
@@ -197,6 +206,7 @@ goToLevel2 character = do
 
 doChoice2 character = do
                                         let newPlayer = move character
+                                        if (isAlive newPlayer == False) then gameOverFail else do
                                         putStrLn "\nYou wake up to a strong throbbing in your head and no memory of where you are or how you got here. You try to look around at your suroundings for any hints as to where you might be, but all you see are glowing orbs of light floating around in the darkness. You get up, and suddenly it hits you. You were abducted by a tractor beam! You realize that you must have been kidnapped by aliens and try your hardest not to freak out. You look around and as your eyes begin to adjust to the darkness, your surroundings begin to come into focus.\nAs you force yourself to calm down and carefully observe your surroundings, you notice that there is a glowing light coming from your right side and strange noises coming from your left.\nAfter your mental struggle with yourself of whether you want to walk towards the GLOWING LIGHT, check out the STRANGE NOISES, or TAKE A STEP AWAY from where you are currently standing so that you can begin to grope around in the darkness, you have come to a decision.\nYou want to..."
                                         input <- getLine
                                         if input == "STATUS" then do
@@ -223,8 +233,9 @@ doChoice2 character = do
                                                     doChoice2 character
                                         
 goToLight str character = do
-                                        putStrLn "\nYou reach out and feel something slimy and smooth. Creeped out, you jump back and look at your hands to see a green electric slime coating your fingertips. You touch the wall in front of you, only to realize that it isn't actually a wall. It feels... slimy... and smooth... Your eyes widden in horror as you begin to frantically rub your hands across the \"wall\", only to have it reveal more of the green electric slime. Your horror increases as it finally hits you. You are trapped inside of a giant, green slime bubble.\nYou frantically grope around your surroundings in hopes that you can find something that will help you out of this mess. Your hands brush against something that feels like a switch. You brace yourself for the worst and flip it.\nImmediately, the slime bubble around you begins to glow brighter and brighter.\nYou look around at your surroundings and see that there are three items stuck to the sides of the bubble: a SCREWDRIVER, a pair of SCISSORS, and a CROWBAR.\nWhat do you want to grab?"
                                         let newPlayer = move character
+                                        if (isAlive newPlayer == False) then gameOverFail else do
+                                        putStrLn "\nYou reach out and feel something slimy and smooth. Creeped out, you jump back and look at your hands to see a green electric slime coating your fingertips. You touch the wall in front of you, only to realize that it isn't actually a wall. It feels... slimy... and smooth... Your eyes widden in horror as you begin to frantically rub your hands across the \"wall\", only to have it reveal more of the green electric slime. Your horror increases as it finally hits you. You are trapped inside of a giant, green slime bubble.\nYou frantically grope around your surroundings in hopes that you can find something that will help you out of this mess. Your hands brush against something that feels like a switch. You brace yourself for the worst and flip it.\nImmediately, the slime bubble around you begins to glow brighter and brighter.\nYou look around at your surroundings and see that there are three items stuck to the sides of the bubble: a SCREWDRIVER, a pair of SCISSORS, and a CROWBAR.\nWhat do you want to grab?"
                                         input <- getLine
                                         if input == "STATUS" then do
                                                                                         let x = (read (numPots newPlayer) :: Int)
@@ -256,8 +267,9 @@ goToLight str character = do
                                                     goToLight str character
 
 goToNoises str character = do
-                                        putStrLn "\nAs you walk towards the strange noises, you begin to recognize them. It's the sound that is created when two metal or steel objects are being banged together. You soon find yourself in front of a door with light streaming out of it. You cautiously open the door and peek inside the room.\nYou see rows of gigantic cylindrical containers lined against the walls, with their lids opening and closing. You also notice that there are three items laying partially hideen behind one of the containers and beyond one of the containers, a metal door. You walk into the room and try to open the door, only to find out that it's locked. You go back to the items and find: a small BOX, a SCREWDRIVER, a pair of SCISSORS, and a CROWBAR.\nWhat do you want to grab?"
                                         let newPlayer = move character
+                                        if (isAlive newPlayer == False) then gameOverFail else do
+                                        putStrLn "\nAs you walk towards the strange noises, you begin to recognize them. It's the sound that is created when two metal or steel objects are being banged together. You soon find yourself in front of a door with light streaming out of it. You cautiously open the door and peek inside the room.\nYou see rows of gigantic cylindrical containers lined against the walls, with their lids opening and closing. You also notice that there are three items laying partially hideen behind one of the containers and beyond one of the containers, a metal door. You walk into the room and try to open the door, only to find out that it's locked. You go back to the items and find: a small BOX, a SCREWDRIVER, a pair of SCISSORS, and a CROWBAR.\nWhat do you want to grab?"
                                         input <- getLine
                                         if input == "STATUS" then do
                                                                                         let x = (read (numPots newPlayer) :: Int)
@@ -291,12 +303,14 @@ goToNoises str character = do
 
 getBox str character
     | str == "true" =   do
+                                    --if (isAlive newPlayer == False) then gameOverFail else do
                                     let newPlayer = extra character
                                     putStrLn "You open the box and find a potion.\nYOU HAVE OBTAINED A POTION!"
                                     let str = "false"
                                     goToNoises str newPlayer
     | str == "false"    =   do
                                         let newPlayer = move character
+                                        if (isAlive newPlayer == False) then gameOverFail else do
                                         putStrLn "You open the box and find nothing."
                                         goToNoises str character
     | otherwise         =   do
@@ -304,8 +318,9 @@ getBox str character
                                         getBox str character
 
 goToStep str character = do
-                                        putStrLn "\nOuch! You bump your head on what feels like a metal bar on the ceiling. You didn't realize that the space you were in was so small. You grope around your surroundings in hopes that you can find something that will help you out of this mess. Your hands brush against something that feels like a switch. You brace yourself for the worst and flip it.\nImmediately, the room gets flooded with light.\nYoulook around at your surroundings and realize that you can only take one step in each direction. You start hyperventilating due to your claustrophobia until you notice that there is a tool kit in one of the corners.\nYou open the tool kit, hoping to find something that will help you escape, and find: a SCREWDRIVER, a pair of SCISSORS, and a CROWBAR.\nWhat do you want to grab?"
                                         let newPlayer = move character
+                                        if (isAlive newPlayer == False) then gameOverFail else do
+                                        putStrLn "\nOuch! You bump your head on what feels like a metal bar on the ceiling. You didn't realize that the space you were in was so small. You grope around your surroundings in hopes that you can find something that will help you out of this mess. Your hands brush against something that feels like a switch. You brace yourself for the worst and flip it.\nImmediately, the room gets flooded with light.\nYoulook around at your surroundings and realize that you can only take one step in each direction. You start hyperventilating due to your claustrophobia until you notice that there is a tool kit in one of the corners.\nYou open the tool kit, hoping to find something that will help you escape, and find: a SCREWDRIVER, a pair of SCISSORS, and a CROWBAR.\nWhat do you want to grab?"
                                         input <- getLine
                                         if input == "STATUS" then do
                                                                                         let x = (read (numPots newPlayer) :: Int)
@@ -344,6 +359,7 @@ goToLevel3 character = do
 
 doChoice3 character = do
                                         let newPlayer = move character
+                                        if (isAlive newPlayer == False) then gameOverFail else do
                                         putStrLn("\nYou find yourself in a long hallway. There is a guard standing near a door who seems like he is dozing off. \"That room must be important if it has a guard in front of it...\" you think to yourself as you approach it.")
                                         putStrLn "Do you want to SCREAM and hope that the loud noise wakes you up from what are hoping is a dream, SURPRISE ATTACK the guard in hopes that your attack will render them unconscious, or SNEAK INSIDE the room?"
                                         input <- getLine
@@ -378,6 +394,7 @@ doChoice3 character = do
 
 goToGuard character = do
                                             let newPlayer = move character
+                                            if (isAlive newPlayer == False) then gameOverFail else do
                                             putStrLn "You sigh in relief and stare down at the guard. You can see something sticking out of their pocket and debate whether or not you want to grab the ITEM or JUST LEAVE."
                                             putStrLn "You can see the guard beginning to stir and quickly decide that you want to..."
                                             input <- getLine
@@ -403,6 +420,7 @@ goToGuard character = do
                                                                             putStrLn "\nYou quickly bend over to grab the item from the guard's pocket. However, upon doing so, your hand brushes against their armor and you feel a jolt of electricity passing throughout your body.\nYou cry out from the excruciating pain despite your best efforts not to as you stagger through the door to the next room.\nYou let out a whimper as you lean against the door and you swear that you can feel your body getting weaker by the second.\nWhat feels like hours pass before you feel well enough to get up and move around again. You take this chance to study your surroundings."
                                                                             --THIS TAKES AWAY 25 HP. CHECK WITH JAVA PROGRAM TO SEE IF THAT IS THE CASE THERE AS WELL
                                                                             let hurtPlayer = extra (hit newPlayer)
+                                                                            if (isAlive newPlayer == False) then gameOverFail else do
                                                                             goFinal hurtPlayer
                                             else if input == "JUST LEAVE" then goFinal newPlayer
                                             else do
@@ -411,6 +429,7 @@ goToGuard character = do
 
 goFinal character = do
                                     let newPlayer = move character
+                                    if (isAlive newPlayer == False) then gameOverFail else do
                                     putStrLn "\nYou find yourself in a large room containing a huge holographic picture of Earth being projected above a white, circular table.\nYou quickly snap out of your awestruck state as you hear footsteps and what sounds like voices getting closer to the room. You panic as you quickly hide behind the table. You rack your brains thinking of what the best course of action is. You see a flashing red BUTTON on the control panel in front of you, a GUN next to you, and a DESK behind you. The door to the room slides open as you dive for the..."
                                     input <-  getLine
                                     if input == "STATUS" then do
